229264ce33050b4099d3a87f99d673f3ab63fb48
pragya singh <pragya.singh@sts.in>
2021-09-07 11:50:34 +0530

    added new element

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 let el = <h1> hello pragya</h1>;
+let el2 = <h1> hello pragya 2</h1>;
 
 ReactDOM.render(el,document.getElementById("root"));
 
